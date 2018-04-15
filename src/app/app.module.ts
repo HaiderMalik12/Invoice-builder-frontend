d08af0a4b77911be80c572506d33e52d5b40b121
Haider Malik <haidermalik504@gmail.com>
2018-04-15 15:53:46 +0500

    create shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-
-import {MatButtonModule, MatIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
 
 
 @NgModule({
@@ -13,8 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports : [
+    MatButtonModule,
+    MatIconModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
