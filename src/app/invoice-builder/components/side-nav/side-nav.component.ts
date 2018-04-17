84912c5bde2e9fba2c93098220ff85c27bec26b5
Haider Malik <haidermalik504@gmail.com>
2018-04-17 14:53:46 +0500

    adding responsiveness to the sidebar

--- src/app/invoice-builder/components/side-nav/side-nav.component.ts
+++ src/app/invoice-builder/components/side-nav/side-nav.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+const MAX_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  private mediaMatcher:MediaQueryList =
+  matchMedia(`(max-width : ${MAX_WIDTH_BREAKPOINT}px)`)
+  constructor(zone: NgZone) {
+    this.mediaMatcher.addListener((mql) => {
+      zone.run(() => this.mediaMatcher = mql)
+    })
+  }
 
   ngOnInit() {
   }
 
+  isScreenSmall(){
+   return this.mediaMatcher.matches;
+  }
 }
