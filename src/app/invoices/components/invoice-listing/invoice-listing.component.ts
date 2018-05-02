457d55b03d5164ec3a9749f1ac8da33524121138
Haider Malik <haidermalik504@gmail.com>
2018-05-02 16:23:33 +0500

    fix and refactor sorting

--- src/app/invoices/components/invoice-listing/invoice-listing.component.ts
+++ src/app/invoices/components/invoice-listing/invoice-listing.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
 import { Router } from '@angular/router';
-import { MatSnackBar, MatPaginator, MatSort } from '@angular/material';
+import { MatSnackBar, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { remove } from 'lodash';
 import 'rxjs/Rx';
+import { of as observableOf } from 'rxjs/observable/of';
+import { catchError } from 'rxjs/operators/catchError';
+import { map } from 'rxjs/operators/map';
+import { startWith } from 'rxjs/operators/startWith';
+import { switchMap } from 'rxjs/operators/switchMap';
+import { merge } from 'rxjs/observable/merge';
 
 
 @Component({
@@ -12,13 +18,14 @@ import 'rxjs/Rx';
   templateUrl: './invoice-listing.component.html',
   styleUrls: ['./invoice-listing.component.scss']
 })
-export class InvoiceListingComponent implements OnInit, AfterViewInit {
+export class InvoiceListingComponent implements OnInit, AfterViewInit, AfterViewChecked {
   constructor(
     private invocieService: InvoiceService,
     private router: Router,
-    private snackBar: MatSnackBar) { }
+    private snackBar: MatSnackBar,
+    private ref: ChangeDetectorRef) { }
   displayedColumns = ['item', 'date', 'due', 'qty', 'rate', 'tax', 'action'];
-  dataSource: Invoice[] = [];
+  dataSource = new MatTableDataSource<Invoice>();
   resultsLength = 0;
   isResultsLoading = false;
 
@@ -33,10 +40,10 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit {
   deleteBtnHandler(id) {
     this.invocieService.deleteInvoice(id)
       .subscribe(data => {
-        const removedItems = remove(this.dataSource, (item) => {
+        const removedItems = remove(this.dataSource.data, (item) => {
           return item._id === data._id
         });
-        this.dataSource = [...this.dataSource];
+        this.dataSource.data = [...this.dataSource.data];
         this.snackBar.open('Invoice deleted', 'Success', {
           duration: 2000
         })
@@ -45,44 +52,81 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
   }
+
+  ngAfterViewChecked() {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    this.ref.detectChanges();
+  }
   ngAfterViewInit() {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    this.paginator
-      .page
-      .flatMap(() => {
-        this.isResultsLoading = true;
-        return this.invocieService.getInvoices({
-          page: this.paginator.pageIndex,
-          perPage: this.paginator.pageSize,
-          sortField: this.sort.active,
-          sortDir: this.sort.direction
+    // this.paginator
+    //   .page
+    //   .flatMap(() => {
+    //     this.isResultsLoading = true;
+    //     return this.invocieService.getInvoices({
+    //       page: this.paginator.pageIndex,
+    //       perPage: this.paginator.pageSize,
+    //       sortField: this.sort.active,
+    //       sortDir: this.sort.direction
+    //     })
+    //   })
+    //   .subscribe(data => {
+    //     this.dataSource = data.docs;
+    //     this.resultsLength = data.total;
+    //     this.isResultsLoading = false;
+    //   }, err => this.errorHandler(err, 'Failed to fetch invoices'));
+
+    // this.sort.sortChange.
+    //   flatMap(() => {
+    //     this.isResultsLoading = true;
+    //     this.paginator.pageIndex = 0;
+    //     return this.invocieService.getInvoices({
+    //       page: this.paginator.pageIndex,
+    //       perPage: this.paginator.pageSize,
+    //       sortField: this.sort.active,
+    //       sortDir: this.sort.direction
+    //     })
+    //   })
+    //   .subscribe((data) => {
+    //     this.dataSource = data.docs;
+    //     this.resultsLength = data.total;
+    //     this.isResultsLoading = false;
+    //   }, err => this.errorHandler(err, 'Failed to fetch invoices'));
+
+    // this.populateInvoices();
+
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0)
+    merge(this.paginator.page, this.sort.sortChange)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          debugger;
+          this.isResultsLoading = true;
+          return this.invocieService.getInvoices({
+            page: this.paginator.pageIndex,
+            perPage: this.paginator.pageSize,
+            sortField: this.sort.active,
+            sortDir: this.sort.direction
+          })
+        }),
+        map(data => {
+          debugger;
+          this.isResultsLoading = false;
+          this.resultsLength = data.total;
+          return data.docs;
+        }),
+        catchError(() => {
+          this.isResultsLoading = false;
+          this.errorHandler('Failed to fetch invoices', 'Error');
+          return observableOf([])
         })
-      })
+      )
       .subscribe(data => {
-        this.dataSource = data.docs;
-        this.resultsLength = data.total;
-        this.isResultsLoading = false;
-      }, err => this.errorHandler(err, 'Failed to fetch invoices'));
-
-    this.sort.sortChange.
-      flatMap(() => {
-        this.isResultsLoading = true;
-        this.paginator.pageIndex = 0;
-        return this.invocieService.getInvoices({
-          page: this.paginator.pageIndex,
-          perPage: this.paginator.pageSize,
-          sortField: this.sort.active,
-          sortDir: this.sort.direction
-        })
+        debugger;
+        this.dataSource.data = data;
       })
-      .subscribe((data) => {
-        this.dataSource = data.docs;
-        this.resultsLength = data.total;
-        this.isResultsLoading = false;
-      }, err => this.errorHandler(err, 'Failed to fetch invoices'));
-
-    this.populateInvoices();
   }
   private populateInvoices() {
     this.isResultsLoading = true
@@ -93,7 +137,7 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit {
       sortDir: this.sort.direction
     }).subscribe(
       data => {
-        this.dataSource = data.docs;
+        // this.dataSource = data.docs;
         this.resultsLength = data.total;
         console.log(data);
       },
