c6b1a3d377c5d15c0d6d1874163f4fd522dffe67
Haider Malik <haidermalik504@gmail.com>
2018-05-08 11:49:31 +0500

    load clients to the data table

--- src/app/clients/components/client-listing/client-listing.component.ts
+++ src/app/clients/components/client-listing/client-listing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
+import { MatTableDataSource } from '@angular/material';
+import { Client } from '../../models/client';
 
 @Component({
   selector: 'app-client-listing',
@@ -7,13 +9,15 @@ import { ClientService } from '../../services/client.service';
   styleUrls: ['./client-listing.component.scss']
 })
 export class ClientListingComponent implements OnInit {
-
+  displayedColumns = ['firstName', 'lastName', 'email'];
+  dataSource = new MatTableDataSource<Client>();
   constructor(private clientService: ClientService) { }
 
   ngOnInit() {
     this.clientService.getClients()
       .subscribe(data => {
         console.log(data)
+        this.dataSource.data = data;
       }, err => console.error(err));
   }
 
