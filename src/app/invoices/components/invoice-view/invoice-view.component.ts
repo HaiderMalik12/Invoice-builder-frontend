aa959872e66f803e19852be19e51d1c6eb3eb192
Haider Malik <haidermalik504@gmail.com>
2018-05-15 19:01:02 +0500

    calculate the total tax value

--- src/app/invoices/components/invoice-view/invoice-view.component.ts
+++ src/app/invoices/components/invoice-view/invoice-view.component.ts
@@ -9,12 +9,24 @@ import { Invoice } from '../../models/invoice';
 })
 export class InvoiceViewComponent implements OnInit {
   invoice: Invoice;
+  total: number;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.data.subscribe((data: { invoice: Invoice }) => {
       this.invoice = data.invoice;
       console.log(this.invoice);
+      if (
+        typeof this.invoice.qty !== 'undefined' &&
+        typeof this.invoice.rate !== 'undefined'
+      ) {
+        this.total = this.invoice.qty * this.invoice.rate;
+      }
+      let salesTax = 0;
+      if (typeof this.invoice.tax !== 'undefined') {
+        salesTax = this.total * this.invoice.tax / 100;
+      }
+      this.total += salesTax;
     });
   }
 }
--- src/app/invoices/services/edit-invoice-resolver.service.ts
+++ src/app/invoices/services/edit-invoice-resolver.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Invoice } from '../models/invoice';
 import { InvoiceService } from './invoice.service';
 import { take, map } from 'rxjs/operators';
@@ -7,26 +12,22 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class EditInvoiceResolverService implements Resolve<Invoice> {
-
-  constructor(private invoiceService: InvoiceService,
-    private router: Router) { }
-  resolve(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<Invoice> {
+  constructor(private invoiceService: InvoiceService, private router: Router) {}
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Invoice> {
     let id = route.paramMap.get('id');
-    return this.invoiceService.getInvoice(id)
-      .pipe(
-        take(1),
-        map(invoice => {
-          debugger;
-          if (invoice) {
-            return invoice;
-          }
-          else {
-            this.router.navigate(['/dashboard', 'invoices']);
-            return null;
-          }
-        })
-      )
+    return this.invoiceService.getInvoice(id).pipe(
+      take(1),
+      map(invoice => {
+        if (invoice) {
+          return invoice;
+        } else {
+          this.router.navigate(['/dashboard', 'invoices']);
+          return null;
+        }
+      })
+    );
   }
-
 }
