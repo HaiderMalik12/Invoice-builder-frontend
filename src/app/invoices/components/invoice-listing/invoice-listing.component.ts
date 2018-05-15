7f04347e768a37c0d2a2073234e14340b85d4237
Haider Malik <haidermalik504@gmail.com>
2018-05-15 18:19:30 +0500

    adding view action item to material menu

--- src/app/invoices/components/invoice-listing/invoice-listing.component.ts
+++ src/app/invoices/components/invoice-listing/invoice-listing.component.ts
@@ -1,8 +1,20 @@
-import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+  ChangeDetectorRef,
+  AfterViewChecked
+} from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
 import { Router } from '@angular/router';
-import { MatSnackBar, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import {
+  MatSnackBar,
+  MatPaginator,
+  MatSort,
+  MatTableDataSource
+} from '@angular/material';
 import { remove } from 'lodash';
 import 'rxjs/Rx';
 import { of as observableOf } from 'rxjs/observable/of';
@@ -12,19 +24,20 @@ import { startWith } from 'rxjs/operators/startWith';
 import { switchMap } from 'rxjs/operators/switchMap';
 import { merge } from 'rxjs/observable/merge';
 
-
 @Component({
   selector: 'app-invoice-listing',
   templateUrl: './invoice-listing.component.html',
   styleUrls: ['./invoice-listing.component.scss']
 })
-export class InvoiceListingComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class InvoiceListingComponent
+  implements OnInit, AfterViewInit, AfterViewChecked {
   constructor(
     private invocieService: InvoiceService,
     private router: Router,
     private snackBar: MatSnackBar,
-    private ref: ChangeDetectorRef) { }
-  displayedColumns = ['item', 'date', 'due', 'qty', 'rate', 'tax', 'action'];
+    private ref: ChangeDetectorRef
+  ) {}
+  displayedColumns = ['item', 'date', 'due', 'action'];
   dataSource = new MatTableDataSource<Invoice>();
   resultsLength = 0;
   isResultsLoading = false;
@@ -38,36 +51,40 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit, AfterView
     this.router.navigate(['dashboard', 'invoices', id]);
   }
   deleteBtnHandler(id) {
-    this.invocieService.deleteInvoice(id)
-      .subscribe(data => {
-        const removedItems = remove(this.dataSource.data, (item) => {
-          return item._id === data._id
+    this.invocieService.deleteInvoice(id).subscribe(
+      data => {
+        const removedItems = remove(this.dataSource.data, item => {
+          return item._id === data._id;
         });
         this.dataSource.data = [...this.dataSource.data];
         this.snackBar.open('Invoice deleted', 'Success', {
           duration: 2000
-        })
-      }, err => this.errorHandler(err, 'Failed to delete invoice'))
+        });
+      },
+      err => this.errorHandler(err, 'Failed to delete invoice')
+    );
   }
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
   filterText(filterValue: string) {
     this.isResultsLoading = true;
-    filterValue = filterValue.trim()
+    filterValue = filterValue.trim();
     this.paginator.pageIndex = 0;
-    this.invocieService.getInvoices({
-      page: this.paginator.pageIndex,
-      perPage: this.paginator.pageSize,
-      sortField: this.sort.active,
-      sortDir: this.sort.direction,
-      filter: filterValue
-    })
-      .subscribe(data => {
-        this.dataSource.data = data.docs;
-        this.resultsLength = data.total;
-        this.isResultsLoading = false;
-      }, err => this.errorHandler(err, 'Failed to filter invoices'))
+    this.invocieService
+      .getInvoices({
+        page: this.paginator.pageIndex,
+        perPage: this.paginator.pageSize,
+        sortField: this.sort.active,
+        sortDir: this.sort.direction,
+        filter: filterValue
+      })
+      .subscribe(
+        data => {
+          this.dataSource.data = data.docs;
+          this.resultsLength = data.total;
+          this.isResultsLoading = false;
+        },
+        err => this.errorHandler(err, 'Failed to filter invoices')
+      );
   }
 
   ngAfterViewChecked() {
@@ -77,7 +94,7 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit, AfterView
   }
   ngAfterViewInit() {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0)
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
     merge(this.paginator.page, this.sort.sortChange)
       .pipe(
         startWith({}),
@@ -89,7 +106,7 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit, AfterView
             sortField: this.sort.active,
             sortDir: this.sort.direction,
             filter: ''
-          })
+          });
         }),
         map(data => {
           this.isResultsLoading = false;
@@ -99,12 +116,12 @@ export class InvoiceListingComponent implements OnInit, AfterViewInit, AfterView
         catchError(() => {
           this.isResultsLoading = false;
           this.errorHandler('Failed to fetch invoices', 'Error');
-          return observableOf([])
+          return observableOf([]);
         })
       )
       .subscribe(data => {
         this.dataSource.data = data;
-      })
+      });
   }
 
   private errorHandler(error, message) {
--- src/app/invoices/invoices.module.ts
+++ src/app/invoices/invoices.module.ts
@@ -7,10 +7,21 @@ import { InvoiceService } from './services/invoice.service';
 import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
 import { EditInvoiceResolverService } from './services/edit-invoice-resolver.service';
 import { InvoiceViewComponent } from './components/invoice-view/invoice-view.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, MaterialModule, ReactiveFormsModule],
-  declarations: [InvoiceListingComponent, InvoiceFormComponent, InvoiceViewComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  declarations: [
+    InvoiceListingComponent,
+    InvoiceFormComponent,
+    InvoiceViewComponent
+  ],
   exports: [InvoiceListingComponent, InvoiceFormComponent],
   providers: [InvoiceService, EditInvoiceResolverService]
 })
