133af781ea81d6aa2e89c57382434131f5194371
Haider Malik <haidermalik504@gmail.com>
2018-05-19 18:33:48 +0500

    design reset password form

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../core';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder,
+             private authService: AuthService) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+  private initForm(){
+    this.form = this.fb.group({
+      password: ['', Validators.required],
+      confirmPassword : ['', Validators.required]
+    })
   }
 
 }
