45ea3fa51d4449b66aa46760c1ffa0c048a9da9c
Haider Malik <haidermalik504@gmail.com>
2018-05-19 18:49:14 +0500

    password match validation

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-reset-password',
@@ -11,10 +12,20 @@ export class ResetPasswordComponent implements OnInit {
 
   form: FormGroup;
   constructor(private fb: FormBuilder,
-             private authService: AuthService) { }
+             private authService: AuthService,
+             private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.initForm();
+  }
+  onSubmit(){
+    let {password, confirmPassword} = this.form.value;
+    if(password !== confirmPassword){
+      this.snackBar.open('Both password should match', 'warning',{
+        duration: 3000
+      });
+      return;
+    }
   }
   private initForm(){
     this.form = this.fb.group({
@@ -22,5 +33,6 @@ export class ResetPasswordComponent implements OnInit {
       confirmPassword : ['', Validators.required]
     })
   }
+
 
 }
